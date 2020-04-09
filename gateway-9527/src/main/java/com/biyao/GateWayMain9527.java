7023370cb08bbde9ecad2913227d051096fa13e6
huangxiaosheng <394480037@qq.com>
2020-04-09 15:39:08 +0800

    feat:网关

---
+++ gateway-9527/src/main/java/com/biyao/GateWayMain9527.java
@@ -0,0 +1,20 @@
+package com.biyao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * 启动器
+ * @description:
+ * @author: hxs
+ * @create: 2020/4/9
+ */
+
+@SpringBootApplication
+@EnableEurekaClient
+public class GateWayMain9527 {
+    public static void main(String[] args) {
+        SpringApplication.run(GateWayMain9527.class, args);
+    }
+} 
