ff8ff8825c6554e1e57a0c20f54bfe2df2d8b2b0
huangxiaosheng <394480037@qq.com>
2020-04-26 18:37:06 +0800

    feat:多租户

---
+++ tenancy/src/main/java/com/biyao/hibernate/MultiTenantConnectionProviderImpl.java
@@ -0,0 +1,38 @@
+package com.biyao.hibernate;
+
+import org.hibernate.engine.jdbc.connections.spi.AbstractDataSourceBasedMultiTenantConnectionProviderImpl;
+
+import javax.sql.DataSource;
+
+/**
+ * 多租户数据源提供者
+ * 这个类是Hibernate框架拦截sql语句并在执行sql语句之前更换数据源提供的类
+ *
+ * @Author tanglh
+ * @Date 2018/11/23 12:47
+ */
+public class MultiTenantConnectionProviderImpl extends AbstractDataSourceBasedMultiTenantConnectionProviderImpl {
+
+    /**
+     * 默认数据源
+     *
+     * @return
+     */
+    @Override
+    protected DataSource selectAnyDataSource() {
+        return selectDataSource(TenantDataSourceProvider.DEFAULT_KEY);
+    }
+
+    /**
+     * 自定义数据源
+     * 不存在则返回默认
+     *
+     * @param tenantIdentifier
+     * @return
+     */
+    @Override
+    protected DataSource selectDataSource(String tenantIdentifier) {
+        return TenantDataSourceProvider.getTenantDataSource(tenantIdentifier);
+    }
+
+}
---
+++ tenancy/src/main/java/com/biyao/hibernate/MultiTenantIdentifierResolver.java
@@ -0,0 +1,26 @@
+package com.biyao.hibernate;
+
+import com.biyao.util.ThreadTenantUtil;
+import org.hibernate.context.spi.CurrentTenantIdentifierResolver;
+
+import java.util.Optional;
+
+/**
+ * 多租户解析器
+ *
+ * @author: hxs
+ * @create: 2020/4/26
+ */
+public class MultiTenantIdentifierResolver implements CurrentTenantIdentifierResolver {
+
+    // 获取tenantId的逻辑在这个方法里面写
+    @Override
+    public String resolveCurrentTenantIdentifier() {
+        return Optional.ofNullable(ThreadTenantUtil.getTenant()).orElse(TenantDataSourceProvider.DEFAULT_KEY);
+    }
+
+    @Override
+    public boolean validateExistingCurrentSessions() {
+        return true;
+    }
+}
