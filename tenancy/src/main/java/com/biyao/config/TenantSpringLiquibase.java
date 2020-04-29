24577453d153f60cdc1589ce038908db52e91f33
huangxiaosheng <394480037@qq.com>
2020-04-29 18:34:19 +0800

    feat:多租户

---
+++ tenancy/src/main/java/com/biyao/config/TenantSpringLiquibase.java
@@ -0,0 +1,45 @@
+package com.biyao.config;
+
+import liquibase.exception.LiquibaseException;
+import liquibase.integration.spring.SpringLiquibase;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.util.StopWatch;
+
+/**
+ * 租户数据源管理
+ */
+public class TenantSpringLiquibase extends SpringLiquibase {
+
+    private final Logger logger = LoggerFactory.getLogger(TenantSpringLiquibase.class);
+
+
+    @Override
+    public void afterPropertiesSet() throws LiquibaseException {
+        boolean canRun = null != dataSource && shouldRun;
+        if (canRun) {
+            logger.debug("Starting Liquibase synchronously");
+            initDb();
+        } else if (null == dataSource) {
+            logger.warn("dataSource is empty.");
+        } else {
+            log.info("Liquibase is not running");
+        }
+    }
+
+    private void initDb() throws LiquibaseException {
+        StopWatch watch = new StopWatch();
+        watch.start();
+        super.afterPropertiesSet();
+        watch.stop();
+        logger.debug("Started Liquibase in {} ms", watch.getTotalTimeMillis());
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        StopWatch watch = new StopWatch();
+        watch.start();
+        Thread.sleep(1000l);
+        watch.stop();
+        System.out.println(watch.getTotalTimeMillis());
+    }
+}
